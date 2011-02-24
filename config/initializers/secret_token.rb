691380431f689a91a0738458c28a8d8bc296b488
Fenec <lisenokfenec@yandex.ru>
2011-02-24 12:39:02 +0300

    clean rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coolwords::Application.config.secret_token = '6773079ac4eeb5d09d897154255ec71929edc06778efde998c712f25a17b0dc8605861bc6f0dc07fa33a5e5f801f68e2c1ff8b4a251e5f8ba1d26ad7d9e34f28'
